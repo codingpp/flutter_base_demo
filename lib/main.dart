a4ec50ecc32db8eae00bf289c27bbefeae8d0317
sunpan <sunpan920@gmail.com>
2020-06-09 21:29:14 +0800

    首页列表写法优化

--- lib/main.dart
+++ lib/main.dart
@@ -33,130 +33,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  _createList() {
-    return <Widget>[
-      new GestureDetector(
-          child: new Card(
-            child: new Container(
-              padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-              child: new Center(
-                child: new Text('如何使用Flutter包和插件?'),
-              ),
-            ),
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              new MaterialPageRoute(builder: (context) => new PluginUse()),
-            );
-          }),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('如何进行Flutter布局开发'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) => new FlutterLayoutPage()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('StateFullWidget与基础组件'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) => new StateFullGroup()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('StatelessWidget与基础组件'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(context,
-              new MaterialPageRoute(builder: (context) => new LessGroupPage()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('编写第一个flutter应用'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) => new FirstFlutterApp()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('网络图片'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(context,
-              new MaterialPageRoute(builder: (context) => new ImagePage()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: new Text('基本List'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(context,
-              new MaterialPageRoute(builder: (context) => new BaseList()));
-        },
-      ),
-      new GestureDetector(
-        child: new Card(
-          child: new Container(
-            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: new Center(
-              child: Text('底部BottomNavigationBar'),
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => TabBarPage()));
-        },
-      )
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -166,8 +42,41 @@ class _MyHomePageState extends State<MyHomePage> {
         body: new Container(
           padding: EdgeInsets.all(15),
           child: ListView(
-            children: _createList(),
+            children: [
+              _renderItem('如何使用Flutter包和插件', () => _jump(PluginUse())),
+              _renderItem('如何进行Flutter布局开发', () => _jump(FlutterLayoutPage())),
+              _renderItem(
+                  'StateFullWidget与基础组件', () => _jump(StateFullGroup())),
+              _renderItem('StatelessWidget与基础组件', () => _jump(LessGroupPage())),
+              _renderItem('编写第一个flutter应用', () => _jump(FirstFlutterApp())),
+              _renderItem('网络图片', () => _jump(ImagePage())),
+              _renderItem('基本List', () => _jump(BaseList())),
+              _renderItem('底部BottomNavigationBar', () => _jump(TabBarPage()))
+            ],
           ),
         ));
+  }
+
+  ///渲染item
+  _renderItem(String title, onTap) {
+    return GestureDetector(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+          child: Center(
+            child: Text(title),
+          ),
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  ///公共跳转方法
+  _jump(Widget page) {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => page),
+    );
   }
 }
